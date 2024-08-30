f129c278ef3376d60c3b2249b7b385d3236f859a
Matheus Uggioni Possamai <166436309+Matheus-u-p@users.noreply.github.com>
2024-08-29 21:29:34 -0300

    Delete Programação Para WEB directory

--- Programação Para WEB/get_post_etc/Atividades_0-9/atv2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$combustivel = $_POST['combustivel'];
-$distancia = $_POST['distancia'];
-
-$mediaconsumo = $distancia / $combustivel;
-
-echo "A media de consumo do automovel é: $mediaconsumo";
-?>
--- Programação Para WEB/get_post_etc/Atividades_0-9/atv4.php
+++
@@ -1,7 +0,0 @@
-<?php
-$celsius = $_POST['v1'];
-
-$F=(9*$celsius+160)/5;
-
-echo("A temperatura em graus celsius $celsius é $F em Fahrenheit");
-?>
